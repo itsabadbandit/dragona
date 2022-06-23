6c4038432bc25559d08fbda265262164dcdf46f8
b4ndit <85434416+itsabadbandit@users.noreply.github.com>
2022-06-23 00:33:49 -0400

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,3 +1,5 @@
 if game.PlaceId == 4866692557 then
     loadstring(game:HttpGet('https://raw.githubusercontent.com/itsabadbandit/dragona/main/games/aoh.lua'))()
+    elseif game.PlaceId == 8884433153 then
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/itsabadbandit/dragona/main/games/cap.lua'))()
 end
